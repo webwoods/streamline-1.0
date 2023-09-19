187be851d5f052da02f5d8ba945cfe5196e68b74
Shihan98 <shihanthathsara98@gmail.com>
2023-09-19 16:07:07 +0530

    add pages.tsx to headerAction

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,12 @@
+'use client'
 import styles from '@/app/page.module.css'
+import { HeaderAction } from '@/components/navbar/navbar'
 
 export default function Home() {
 
   return (
     <>
+    <HeaderAction/>
       <main className={styles.main}>
         
       </main>
