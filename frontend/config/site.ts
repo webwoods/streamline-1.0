02bb50b6cbf0544324e4e49342ba0d3eb65c73f0
kodiidok <sahan.crkodituwakku@gmail.com>
2023-09-26 15:41:22 +0530

    feat: set page layout to max width 1200px

--- frontend/config/site.ts
+++ frontend/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
+	name: "Stream Line",
 	description: "Make beautiful websites regardless of your design experience.",
 	navItems: [
 		{
