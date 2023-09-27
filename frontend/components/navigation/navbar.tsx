5b39bae80beaac9f0cb2268d0a8de6642fa76415
kodiidok <sahan.crkodituwakku@gmail.com>
2023-09-27 11:56:59 +0530

    feat: responsive navbar

--- frontend/components/navigation/navbar.tsx
+++ frontend/components/navigation/navbar.tsx
@@ -1,9 +1,14 @@
+'use client'
+
 import UserProfileButton from "@/components/navigation/userProfileButton"
 import styles from '@/styles/navigation.module.css'
 import { Notification } from "./notification";
+import { useState } from "react";
+import { ThreeLineHorizontal } from "akar-icons";
 
 export default function Navbar() {
   const tabs = ['Dashboard', 'Request', 'Files'];
+  const [isMobile, setIsMobile] = useState(false);
 
   return (
     <div className={styles['ww-navbar']}>
@@ -16,6 +21,21 @@ export default function Navbar() {
             )
           })}
         </div>
+
+        {
+          <div>
+            <ThreeLineHorizontal className={styles['menu-icon']} size={36} color={'white'} onClick={() => {setIsMobile(!isMobile)}} />
+            {isMobile && (
+            <div className={styles['ww-navbar-items-mobile']}>
+              {tabs.map((tabData) => {
+                return (
+                  <div className={styles['ww-navbar-item-mobile']} key={tabData}>{tabData}</div>
+                )
+              })}
+            </div>
+            )}
+          </div>
+        }
 
         {/* user profile and notification */}
         <div>
