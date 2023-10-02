53b9e3226ed6cbdcf05d61c863ac552d777a7e7e
ChamudithaGuruge <chamuditha2244@gmail.com>
2023-10-02 07:30:12 +0530

    layout,page and module

--- frontend/app/pricing/page.tsx
+++ frontend/app/pricing/page.tsx
@@ -3,7 +3,7 @@ import { title } from "@/components/primitives";
 export default function PricingPage() {
 	return (
 		<div>
-			<h1 className={title()}>Pricing</h1>
+			<h1 className={title()}>Pricindsg</h1>
 		</div>
 	);
 }
---
+++ frontend/components/forms/CustomizableForm/CustomizableForm.tsx
@@ -0,0 +1,18 @@
+//import AddNew from "./addNew";
+
+import AddNew from "./AddNew";
+import FormAttribute from "./FormAttribute";
+
+export default function CustomizableForm() {
+    return (
+        
+        <h1>Hello</h1>
+
+            ) 
+
+}
+
+
+
+
+
