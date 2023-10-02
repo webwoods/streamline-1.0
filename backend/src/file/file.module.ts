4aba6f015664c9c2ac5b91cd1fb686d4970d8a66
ChamudithaGuruge <chamuditha2244@gmail.com>
2023-10-02 20:20:40 +0530

    File

--- backend/src/file/file.module.ts
+++ backend/src/file/file.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { FileResolver } from './file/file.resolver';
+import { FileService } from './file/file.service';
 
-@Module({})
+@Module({
+  providers: [FileResolver, FileService]
+})
 export class FileModule {}
