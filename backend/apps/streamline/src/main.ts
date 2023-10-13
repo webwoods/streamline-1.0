514040bf78b169b3595e152f6cc5257f337f7421
ChamudithaGuruge <chamuditha2244@gmail.com>
2023-10-13 20:05:00 +0530

    Config demo app

--- backend/apps/streamline/src/main.ts
+++ backend/apps/streamline/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MAIN_APP } from '@libs/core/constants/appInfo';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = MAIN_APP.name;
+  const graphqlEndpoint = MAIN_APP.graphqlEndpoint;
+  app.setGlobalPrefix(globalPrefix);
+  const port = process.env.PORT || MAIN_APP.port;
+  await app.listen(port);
+  Logger.log(
+    `
+    🚀 Application is running on: http://localhost:${port}/${globalPrefix}
+    🚀 Graphql Server is running on: http://localhost:${port}${graphqlEndpoint}
+    🚀 Graphql Playground is running on: http://localhost:${port}${graphqlEndpoint}/playground
+    `
+  );
 }
 bootstrap();
