49d26cdd02d760848ead3daba903c900cf2c56b6
ChamudithaGuruge <chamuditha2244@gmail.com>
2023-10-13 20:23:23 +0530

    Config demo app

--- backend/apps/streamline/src/app.module.ts
+++ backend/apps/streamline/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppService } from './app.service'
+import { AuthModule } from './auth/auth.module';
+import { RequestItemsModule } from './request-items/request-items.module';
+import { ProcurementUserModule } from './procurement-user/procurement-user.module';
+import { FileModule } from './files/file.module';
+import { RequestModule } from './requests/request.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ProcurementUserModule,
+    FileModule,
+    RequestItemsModule,
+    RequestModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
