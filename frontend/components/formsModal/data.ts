05befe549ea7bde5bd8ce59526c41b49a19ac092
Shihan98 <shihanthathsara98@gmail.com>
2023-10-19 08:17:00 +0530

    fixed the dropdown button errors

---
+++ frontend/components/formsModal/data.ts
@@ -0,0 +1,7 @@
+import React from "react";
+export const animals = [
+  {label: "Active", value: "active"},
+  {label: "Pending", value: "pending"},
+  {label: "Paused", value: "paused"},
+  {label: "Cancel", value: "cancel"},
+];
