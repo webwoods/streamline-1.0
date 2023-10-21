e9f773cf66773c28985dc29827d6d84b1d0aa263
Shihan98 <shihanthathsara98@gmail.com>
2023-10-21 17:56:20 +0530

    database setting

--- backend/libs/core/src/config/orm.config.ts
+++ backend/libs/core/src/config/orm.config.ts
@@ -34,7 +34,7 @@ export function getDbConfig({
     host: process.env['DB_HOST'] ?? 'localhost',
     port: Number(process.env['DB_PORT']) || 5432,
     username: process.env['DB_USERNAME'] ?? 'postgres',
-    password: process.env['DB_PASSWORD'] ?? 'pwd@kodi',
+    password: process.env['DB_PASSWORD'] ?? '1234',
     database: process.env['DB_NAME'] ?? 'streamline',
   };
 
