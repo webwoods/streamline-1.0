51f1772d628f677290043d117029d7d7b641aef6
kodiidok <sahan.crkodituwakku@gmail.com>
2023-12-19 07:14:44 +0530

    fix: imports in request items service

--- backend/libs/core/src/services/request-items.service.ts
+++ backend/libs/core/src/services/request-items.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { ILike, Like, Repository } from 'typeorm';
+import { ILike, Repository } from 'typeorm';
 import { RequestItem } from '../entities/request-items.entity';
 
 @Injectable()
