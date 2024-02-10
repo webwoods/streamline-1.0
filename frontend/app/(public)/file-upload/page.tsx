37293e963d09edd3f5314f8d75d1462adf2d8dd0
Yapa-Lanka-Developers <devdilanka@gmail.com>
2024-02-10 17:37:21 +0530

    added drag and drop

---
+++ frontend/app/(public)/file-upload/page.tsx
@@ -0,0 +1,10 @@
+import UploadButton from '@/components/fileUpload/UploadButton'
+import React from 'react'
+
+function page() {
+  return (
+    <div><UploadButton className={''}/></div>
+  )
+}
+
+export default page
