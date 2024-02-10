06885b60da5d26180cc83391858b96baa184636e
kodiidok <sahancr.kodituwakku@gmail.com>
2024-02-10 23:46:48 +0530

    feat: request a store item from store page

--- frontend/components/query/queryStoreItems.tsx
+++ frontend/components/query/queryStoreItems.tsx
@@ -2,16 +2,12 @@
 
 import EmptyPage from "@/app/(public)/empty";
 import client from "@/gql/client";
-import { STORE_ITEMS_QUERY, VENDORS_QUERY } from "@/gql/query";
-import { useLazyQuery, useQuery } from "@apollo/client";
+import { STORE_ITEMS_QUERY } from "@/gql/query";
+import { useLazyQuery } from "@apollo/client";
 import { useCallback, useEffect, useState } from "react";
-import VendorCard from "../vendors/vendors";
-import { Button, Divider, Pagination } from "@nextui-org/react";
+import { Pagination } from "@nextui-org/react";
 import StoreItemCard from "../vendors/storeItems";
-import { formButtonStyles } from "../forms/styles";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark } from "@fortawesome/free-solid-svg-icons";
-import ToolTipButton from "../common/tooltipButton";
+import { StoreItemDetails } from "../vendors/storeItemDetails";
 
 interface Props {
     page: number
@@ -35,12 +31,12 @@ export default function QueryStoreItems({
     const [getStoreItems, { loading, error, data }] = useLazyQuery(STORE_ITEMS_QUERY, { client });
 
     const handlePaginationChange = (page: number) => {
-        console.log(page);
+        // console.log(page);
         setVisiblePage(page);
     }
 
     const viewStoreItemData = useCallback((data: any) => {
-        console.log(data);
+        // console.log(data);
         setActiveStoreItem(data);
     }, [])
 
@@ -108,66 +104,7 @@ export default function QueryStoreItems({
                 </div>
             </div>
             <div className="col-span-1">
-                <div className="flex flex-col items-center px-5">
-                    <div className="w-full flex justify-between items-center">
-                        <span className="text-md font-semibold">Product details</span>
-                        <ToolTipButton
-                            content={"deselect product"}
-                            icon={faCircleXmark}
-                            onClick={clearActiveItemSelection}
-                            isDisabled={!activeStoreItem ? true : false}
-                        />
-                    </div>
-                    <div className="flex flex-col items-center w-full">
-                        {activeStoreItem === null ? (
-                            <div className="w-full flex start">
-                                <p>Click on a product to view details.</p>
-                            </div>
-                        ) : (
-                            <>
-                                <div className="py-5 flex flex-col gap-1 w-full">
-                                    <span>Name: {activeStoreItem.name}</span>
-                                    <span>SKU: {activeStoreItem.sku}</span>
-                                    <span>Type: {activeStoreItem.type}</span>
-                                    <span>Price: {activeStoreItem.price} per {activeStoreItem.unit}</span>
-                                    <span>{activeStoreItem.stock > 0 ? 'In stock' : 'Out of stock'}</span>
-                                </div>
-                                <Button
-                                    radius="none"
-                                    className={formButtonStyles.primary}
-                                    isDisabled={activeStoreItem === null ? true : false}
-                                >
-                                    Request
-                                </Button>
-                                <Divider className="mb-5" />
-
-                                <span className="w-full font-semibold text-slate-500">Additional info</span>
-                                <div className="w-full py-5 flex flex-col gap-1">
-                                    {
-                                        activeStoreItem.properties ? (activeStoreItem.properties.map((property: any, index: number) => {
-                                            return (
-                                                <p key={property.id}>{JSON.stringify(property)}</p>
-                                            )
-                                        })) : ''
-                                    }
-                                </div>
-
-                                <Divider className="mb-5" />
-
-                                <span className="w-full font-semibold text-slate-500">Vendors who sell this product</span>
-                                <div className="w-full py-5 flex flex-col gap-1">
-                                    {
-                                        activeStoreItem.vendors ? (activeStoreItem.vendors.map((vendor: any, index: number) => {
-                                            return (
-                                                <p key={vendor.id}>{JSON.stringify(vendor)}</p>
-                                            )
-                                        })) : 'There are no known vendors who sell this product.'
-                                    }
-                                </div>
-                            </>
-                        )}
-                    </div>
-                </div>
+                <StoreItemDetails data={activeStoreItem} onClear={clearActiveItemSelection} />
             </div>
         </div>
     );
